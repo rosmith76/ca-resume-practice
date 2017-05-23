c15dc58daa4bc9fee2a5d525a0f48b34914f1294
rosmith76 <robbe.smith@gmail.com>
2017-05-23 11:37:47 -0400

    Adding new files from CA practice session Worked on basic HTML CSS

---
+++ index.js
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link type="text/css" rel="stylesheet" href="stylesheet.css"/>
+		<title></title>
+	</head>
+	<body>
+		<div id="header">
+			<p id="name">Robbe Smith</p>
+			<a href="mailto:robbe.smith@gmail.com"><p id="email">robbe.smith@gmail.com</p></a>
+		</div>
+		<div class="left"></div>
+		<div class="right">
+			<h4>Objective</h4>
+			<p>To take a position as a software engineer.</p>
+			<h4>Experience</h4>
+			<p>Junior Developer, Software Company (2016 - Present)</p>
+			<ul>
+				<li>Designed and implemented end-user features for Flagship Product</li>
+				<li>Wrote third-party JavaScript and Ruby libraries</li>
+			</ul>
+			<h4>Skills</h4>
+			<p>Languages: C#, JavaScript, Python, Ruby</p>
+			<p>Frameworks: .NET, Node.js, Django, Ruby on Rails</p>
+			<h4>Education</h4>
+			<p>BA, English, Providence College</p>
+			<ul>
+				<li>Award for best beer pong team</li>
+				<li>GPA: 0.8</li>
+			</ul>
+		</div>
+		<div id="footer">
+			<p>70 Everett St, Arlington, MA 02474 | Tel: (240) 722-7591</p>
+		</div>
+	</body>
